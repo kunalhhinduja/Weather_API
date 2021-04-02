eb765f1c7906af63b09ba118d2526f3fc3a999c0
kunal.hinduja <kunal.hinduja@accenture.com>
2021-04-02 17:06:05 +0530

    Same city & Incorrect city functionality done

--- script.js
+++ script.js
@@ -6,29 +6,47 @@ const form = document.getElementById('form');
 const cityName = document.getElementById('city-name');
 const submitButton = document.getElementById('submit-btn');
 const result = document.getElementById('result');
+const visited = document.getElementById('visited');
+
+let checkedCities = [];
 
 async function findWeather(e) {
 
     e.preventDefault();
 
     let str = cityName.value;
-    let city = str.charAt(0).toUpperCase() + str.slice(1);
-    console.log(city);
+    let city = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&apikey=d354d82d3d3c4099f1701e50080bdd68`);
-    const data = await res.json();
-    // console.log(data);
+    if(checkedCities.includes(city)) 
+    {
+        visited.innerHTML = `<h4> You've already checked the temperature of ${city}. 
+                             Check some where else's temperature! </h4>`;
+        cityName.value = '';
+    }
+    else 
+    {
+        visited.innerHTML = '';
+        checkedCities.push(city);
 
-    const countryInitials = data.sys.country;
-    const temparture = data.main.temp;
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&apikey=d354d82d3d3c4099f1701e50080bdd68`);
+        const data = await res.json();
 
-    console.log(countryInitials);
-    console.log(temparture);
+        if(data.cod == 404 && data.message == 'city not found')
+        {
+            visited.innerHTML = `<h4>Enter proper name of the city!!</h4>`;
+        }
+        else 
+        {  
+        const countryInitials = data.sys.country;
+        const temparture = data.main.temp;
 
-    updateUI(city, countryInitials, temparture);
+        updateUI(city, countryInitials, temparture);
 
-    cityName.value ='';
+        cityName.value = '';
+        }
 
+
+    }
 }
 
 function updateUI(city, countryInitials, temparture) {
@@ -36,9 +54,9 @@ function updateUI(city, countryInitials, temparture) {
     const item = document.createElement('div');
     item.classList.add('information-div');
     item.innerHTML = `
-    <p>City :${city}</p>
-    <p>Country :${countryInitials}</p>
-    <p>Temperature :${temparture}</p>
+    <p class="cityName">${city}</p>
+    <p class="country">${countryInitials}</p>
+    <p>${temparture} <span class="super">°C</span></p>
     `;
     
     result.appendChild(item);
